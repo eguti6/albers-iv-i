a69aaec544779693c4e3b67528d4ca0bbb11590d
eguti6 <98439871+eguti6@users.noreply.github.com>
2022-02-09 17:00:30 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
 
 let clrs = [
-'#E27F34', //top 0
-'#FEF254', //middle top 1
-'#A36C33', //middle strip 2
-'#042060', //middle bottom 3
+'#7133FF', //top 0
+'#D85021', //middle top 1
+'#D821C2', //middle strip 2
+'#D82164', //middle bottom 3
 '#3778AE' //bottom 4
 ]
 
@@ -15,8 +15,8 @@ function setup() {
 
 function draw() {
  background(220);
- canvasRect(clrs[4], height/2, height/2, false);
- canvasRect(clrs[0], height/2, 0, false);
+ canvasRect(clrs[4], height/4, height/2, false);
+ canvasRect(clrs[0], height/3, 0, false);
  fill(clrs[2]); 
  rect(width * 1/3, height * 1/6, width * 1/3, height * 2/3); 
  canvasRect(clrs[1], height/6, height/3, true);
